7164819eaadbb9dec9a17b6debef25bb680f1d51
GreenAnimations <84263084+GreenAnimations755@users.noreply.github.com>
2022-11-21 00:43:03 -0800

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,13 @@
+local MsgMaker = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenAnimations755/Message-Maker/main/Source.lua"))()
+
+MsgMaker.MakeMessage({
+  Text = "Message",
+  Font = Enum.Font.Arial,
+  Duration = 2.5
+})
+
+MsgMaker.MakeHint({
+  Text = "Hint",
+  Font = Enum.Font.Arial,
+  Duration = 2.5
+})
